a2c17145d12c283913b03dcfb74534b7370b9462
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-18 17:48:02 +0100

    css palindroma

--- palindroma/js/palindroma.js
+++ palindroma/js/palindroma.js
@@ -1,22 +1,43 @@
 // Chiedere all’utente di inserire una parola.
 // Creare una funzione per capire se la parola inserita è palindroma.
 
-var userWord = prompt("Prego inserire una parola");
 
-if ( !isNaN(userWord) ) {
-    alert("Per favore inserisci una parola")
-} else {
-    console.log(userWord);
-    var wordReverse = reverseWord(userWord);
-    console.log(wordReverse);
+var controlBtn = document.getElementById("control_btn");
+var deleteBtn = document.getElementById("delete_btn");
 
-    if (userWord == wordReverse) {
-        console.log("La parola da te inserita è palindroma");
-    } else {
-        console.log("La parola da te inserita non è palindroma");
+control_btn.addEventListener("click",
+    function() {
+
+        var userWord = document.getElementById("word").value;
+        if (userWord == "" || !isNaN(userWord)) {
+            alert("Inserisci una parola");
+        } else {
+
+            document.getElementById("your_word").innerHTML = "La parola da te inserita è: " + "<br>" + "<b>" + userWord + "</b>";
+            var wordReverse = reverseWord(userWord);
+            document.getElementById("your_word_reverse").innerHTML = "La parola inserita scritta al contrario diventa: " + "<br>" + "<b>" + wordReverse + "</b>";
+
+            if (userWord == wordReverse) {
+                document.getElementById("is_palindrome").innerHTML = "La parola inserita è palindroma";
+            } else {
+                document.getElementById("is_palindrome").innerHTML = "La parola inserita NON è palindroma";
+            }
+
+        }
+
     }
+);
 
-}
+deleteBtn.addEventListener("click",
+    function() {
+
+        document.getElementById("word").value = "";
+        document.getElementById("your_word").innerHTML = "";
+        document.getElementById("your_word_reverse").innerHTML = "";
+        document.getElementById("is_palindrome").innerHTML = "";
+
+    }
+)
 
 //--------------------------------------------------------------
 
