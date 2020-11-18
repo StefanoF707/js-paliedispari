7fa447747d6ad8db659bbb2394d390f74e5a4bae
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-18 20:49:32 +0100

    css pari-dispari-parziale

--- pari-e-dispari/js/pari-dispari.js
+++ pari-e-dispari/js/pari-dispari.js
@@ -4,36 +4,64 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione).
 // Dichiariamo chi ha vinto.
 
-var evenOdd = prompt("Pari o dispari??");
 
-if (evenOdd == "pari" || evenOdd == "dispari") {
+var playBtn = document.getElementById("go_btn");
+var deleteBtn = document.getElementById("delete_btn");
 
-    console.log("La tua scelta è:", evenOdd);
-    var userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
-    var computerNumber = numberGenerator();
+go_btn.addEventListener("click",
+    function() {
 
-    if (userNumber >= 1 && userNumber <= 5) {
-        console.log("Hai scelto il numero:", userNumber);
-        console.log("Il numero scelto dal computer è:", computerNumber);
+        var userChoise = document.getElementById("even_odd").value;
+        var userNumber = document.getElementById("number").value;
 
-        var sum = userNumber + computerNumber;
-        console.log("La somma dei numeri è:", sum);
-
-        var winner = evenOrOdd(userNumber, computerNumber);
-
-        if (winner == evenOdd) {
-            console.log("HAI VINTO!!");
+        if (userChoise == "not_work") {
+            alert("Per favore scegli una delle due opzioni");
         } else {
-            console.log("Purtroppo hai perso, ritenta!");
+
+            if (userNumber < 1 || userNumber > 5) {
+                alert("Inserisci un numero da 1 a 5");
+            } else {
+
+                var choise1 = "PARI";
+                var choise2 = "DISPARI";
+                if (userChoise == "even") {
+                    document.getElementById("human_choise").innerHTML = "La tua scelta è:" + "<br>" + "<b>" + choise1 + "</b>";
+                    document.getElementById("computer_choise").innerHTML = "La tua scelta è:" + "<br>" + "<b>" + choise2 + "</b>";
+                } else if (userChoise == "odd") {
+                    document.getElementById("human_choise").innerHTML = "La tua scelta è:" + "<br>" + "<b>" + choise2 + "</b>";
+                    document.getElementById("computer_choise").innerHTML = "La tua scelta è:" + "<br>" + "<b>" + choise1 + "</b>";
+                }
+
+                var computerNumber = numberGenerator();
+
+                document.getElementById("human_number").innerHTML = "Il tuo numero è:" + "<br>" + "<b>" + userNumber + "</b>";
+                document.getElementById("computer_number").innerHTML = "Il tuo numero è:" + "<br>" + "<b>" + computerNumber + "</b>";
+
+                var sum = parseInt(userNumber) + parseInt(computerNumber);
+                document.getElementById("numbers_result").innerHTML = "La somma dei numeri è:" + "<br>" + "<b>" + sum + "</b>";
+
+                var evenOdd = evenOrOdd(userNumber, computerNumber);
+                var finalResult;
+                if (evenOdd == "even") {
+                    finalResult = "PARI";
+                } else {
+                    finalResult = "DISPARI";
+                }
+                document.getElementById("choises_result").innerHTML = "Pari o Dispari:" + "<br>" + "<b>" + finalResult + "</b>";
+
+                if (evenOdd == userChoise) {
+                    document.getElementById("is_winner").innerHTML = "HAI VINTO!";
+                } else {
+                    document.getElementById("is_winner").innerHTML = "HAI PERSO!";
+                }
+
+            }
         }
 
-    } else {
-        alert("Per favore scegli un numero compreso tra 1 e 5");
     }
+)
 
-} else {
-    alert("per favore inserisci una delle due scelte")
-}
+
 
 //--------------------------------------------------------------------
 
@@ -42,9 +70,9 @@ function numberGenerator() {
 }
 
 function evenOrOdd(num1, num2) {
-    var result = "dispari";
+    var result = "odd";
     if ( ((num1 + num2) % 2) == 0) {
-        result = "pari";
+        result = "even";
     }
     return result;
 }
